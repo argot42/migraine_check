3bef47e6897345a3fadea319cda0eefd501fe19c
argot <pablotesari@gmail.com>
2017-01-16 12:31:38 -0300

    multiple years accepted, diet and other stuff deleted for now

--- create_graph.py
+++ create_graph.py
@@ -2,72 +2,114 @@ import numpy as np
 import datetime
 import sys
 import sqlite3
+import math
 import bokeh.plotting, bokeh.models
 
-def plot_migr_month(year):
+def plot_migr_month(start_year, end_year=None):
     try:
-            year_as_datetime = datetime.datetime.strptime(year, '%Y')
-            migraine_per_month = get_migraine_year(year_as_datetime, sys.argv[1])
+            startyear = datetime.datetime.strptime(start_year, '%Y')
+            endyear = (end_year and datetime.datetime.strptime(end_year, '%Y')) or startyear
+            migraines = get_migraine_year(startyear, endyear, sys.argv[1])
     except:
         raise
 
     bokeh.plotting.output_file("graph.html")
 
-    # x axis
-    x = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
-
-    # y axis
-    # count of migraines per month
-    y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for i in migraine_per_month: 
-        migrained = datetime.datetime.strptime(i[0], "%Y-%m-%d %H:%M:%S")
-        y[migrained.month - 1] += 1
+    # get x axis names
+    x = getmonths(startyear.year, endyear.year)
+    # get y axis values
+    y, intensity = migraine_info(startyear.year, endyear.year, migraines)
 
     # source for tooltip and plot
     # process daily info
-    diet, sleeptime, comment = get_dailyinfo_year(year_as_datetime, sys.argv[1])
+    #diet, sleeptime, comment = get_dailyinfo_year(startyear, sys.argv[1])
+
     source = bokeh.plotting.ColumnDataSource(
             data=dict(
                 x=x,
                 y=y,
-                **diet,
-                sleeptime=sleeptime,
-                comment=comment,
+                intensity=intensity,
+                #**diet,
+                #sleeptime=sleeptime,
+                #comment=comment,
             )
         )
 
-    # convert diet for hover tool
-    diet4tooltip = tooltip_transform(diet) 
     # Hover tool
     hover = bokeh.models.HoverTool(
             tooltips=[
                 ("Mes", "@x"),
                 ("Migrañas", "@y"),
-                ("Tiempo de Sueño", "@sleeptime"),
-                ("Comentarios", "@comment"),
-                *diet4tooltip
-            ]
-        )
+                ("Intensidad", "@intensity")
+                ]
+            )
 
-    p = bokeh.plotting.figure(plot_width=1200, plot_height=900, x_range=x, tools=[hover], title="Migrañas por Mes")
+    ## convert diet for hover tool
+    #diet4tooltip = tooltip_transform(diet) 
+    ## Hover tool
+    #hover = bokeh.models.HoverTool(
+    #        tooltips=[
+    #            ("Mes", "@x"),
+    #            ("Migrañas", "@y"),
+    #            ("Tiempo de Sueño", "@sleeptime"),
+    #            ("Comentarios", "@comment"),
+    #            *diet4tooltip
+    #        ]
+    #    )
+
+    p = bokeh.plotting.figure(plot_width=800, plot_height=600, x_range=x, tools=[hover], title="Migrañas por Mes")
 
     p.line(x, y, line_width=2)
     p.circle('x', 'y', fill_color="white", size=8, alpha=0.5, source=source)
+
+    p.xaxis.major_label_orientation = math.pi/2
     bokeh.plotting.show(p)
 
 
-def get_migraine_year(year, db):
+def getmonths(syear, eyear):
+    months = []
+    for year in range(syear, eyear + 1):
+        months.append("Enero " + str(year))
+        months.append("Febrero " + str(year))
+        months.append("Marzo " + str(year))
+        months.append("Abril " + str(year))
+        months.append("Mayo " + str(year))
+        months.append("Junio " + str(year))
+        months.append("Julio " + str(year))
+        months.append("Agosto " + str(year))
+        months.append("Septiembre " + str(year))
+        months.append("Octubre " + str(year))
+        months.append("Noviembre " + str(year))
+        months.append("Diciembre " + str(year))
 
+    return months
+
+
+def migraine_info(syear, eyear, migraines):
+    migraines_per_month = [0] * (12 * (eyear - syear + 1))
+    intensity_per_month = [0] * (12 * (eyear - syear + 1))
+
+    for migraine in migraines:
+        # count migraine
+        migrained = datetime.datetime.strptime(migraine[0], "%Y-%m-%d %H:%M:%S")
+        migraines_per_month[migrained.month - 1] += 1
+
+        # calculate total intensity
+        intensity_per_month[migrained.month -1] += migraine[2]   
+
+    return migraines_per_month, intensity_per_month
+
+
+def get_migraine_year(syear, eyear, db):
     with sqlite3.connect(db) as conn:
-        # get migraines between year and year++
+        # get migraines between syear and eyear
         res = conn.execute("SELECT start, duration, intensity, comment FROM migraine WHERE start >= date(?) AND start < date(?)", \
-                (year.isoformat(), (year+datetime.timedelta(days=365)).isoformat()))
+                (syear.isoformat(), (eyear+datetime.timedelta(days=365)).isoformat()))
 
-        return res.fetchall()
+    return res.fetchall()
 
     
 def get_dailyinfo_year(year, db):
-
     with sqlite3.connect(db) as conn:
         # get daily info between year and year++
         res = conn.execute("SELECT day.date, day.sleep_time, day.comment, food.type FROM day LEFT OUTER JOIN daily_menu ON day.date = daily_menu.day_id LEFT OUTER JOIN food ON daily_menu.food_id = food.type WHERE day.date >= date(?) AND day.date < date(?)", \
@@ -126,4 +168,4 @@ def tooltip_transform(diet):
 
 
 if __name__ == '__main__':
-    plot_migr_month('2016')
+    plot_migr_month('2016', '2017')
